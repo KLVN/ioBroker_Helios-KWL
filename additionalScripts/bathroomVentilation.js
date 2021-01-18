651a525bd8413d308e61a58388a275aea58b8107
KLVN <KLVN@users.noreply.github.com>
2021-01-18 15:28:37 +0100

    Better analysis if shower is running to mitigate false positives

--- additionalScripts/bathroomVentilation.js
+++ additionalScripts/bathroomVentilation.js
@@ -1,10 +1,14 @@
 // BATHROOM VENTILATION ////////////////////////////////////////////////////
 /* I'm monitoring my power consumption and have an instant water heater that consumes a lot of electrical power to heat water.
-   During showers the consumption settles around 6.5 kW and therefore this script will look for the mean power consumption in
-   the last 5 minutes and turn on the ventilation system for the next 15 minutes if it exceeds 6 kW.
-   This helps me avoiding fogged up mirrors and surfaces and mitigates mold and moisture.
-   Because only the overall power consumption is taken into account there may be false positives, e.g. during cooking with multiple
-   (electrical) stove plates or using many high-power devices simultaneously (hair dryer, vacuum cleaner, dish washer, washing machine, ...)
+   During showers the consumption settles around 6.5 kW and therefore this script will query the count of all values in the last
+   three minutes that exceeded 6 kW and turn on the ventilation system for the next 15 minutes. Because I'm logging my consumption
+   every 10 seconds there will be a maximum of 18 values in three minutes and the threshold is set to 14 values for a bit of
+   tolerance. This helps me avoiding fogged up mirrors and surfaces and mitigates mold and moisture.
+   
+   Initially I was querying the mean value but this led to false positives because e.g. washing hands, what happens pretty often
+   during COVID-19, consumes a lot of power (~14 kW) for a very short duration so that the mean surpassed the given threshold.
+   Because only the overall power consumption is taken into account there still may be false positives, e.g. during cooking with multiple
+   (electrical) stove plates or using many high-power devices simultaneously (hair dryer, vacuum cleaner, dish washer, washing machine, ...).
 
    Requirements:
      - Measurement for electrical power consumption
@@ -15,15 +19,14 @@
 */
 
 setInterval(function () {
-  sendTo("influxdb.0", "query", 'SELECT mean("value") FROM "INSERT_STATE_OF_POWER_CONSUMPTION" WHERE time >= now() - 5m GROUP BY time(5m) fill(none)', function (result) {
+  sendTo("influxdb.0", "query", 'SELECT count("value") FROM "INSERT_STATE_OF_POWER_CONSUMPTION" WHERE time >= now() - 3m AND value >= 6000', function (result) {
     if (result.error) {
       console.error(result.error);
     } else {
-      var lastAverages = result.result[0];
-      var lastAverage = lastAverages[lastAverages.length - 1]["mean"];
+      var countAboveThreshold = result["result"][0][0]["count"];
 
-      // If mean power consumption in the last 5 minutes was above 6 kilowatts
-      if (lastAverage >= 6000) {
+      // If there are >= 14 values above 6kW in the last three minutes (see query)
+      if (countAboveThreshold >= 14) {
         // then set fan speed to 4
         var currentFanSpeed = getState(datapoint_prefix + "." + datapoint_names["w00102"])["val"];
         setState(datapoint_prefix + "." + datapoint_names["w00102"], 4);
@@ -33,6 +36,5 @@ setInterval(function () {
         });
       }
     }
-  }
-  );
-}, 310000);
+  });
+}, 180000);
